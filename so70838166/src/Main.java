c6fa91b19a76b12249d173689cdc81acd58e6fb0
RobbingDaHood <he@nykredit.dk>
2022-01-24 18:55:20 +0100

    70838166

---
+++ so70838166/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    // https://stackoverflow.com/questions/70838166/how-to-keep-two-decimals-even-if-the-second-decimal-is-a-zero
+
+    public static void main(String[] args) {
+        System.out.printf("%.2f", 2.0000);
+    }
+}
